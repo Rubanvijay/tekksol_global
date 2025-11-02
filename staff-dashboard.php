035b427709c9fe3608ab020436c489f97de39684
Ruban <rubanvijay1000@gmail.com>
2025-11-02 11:49:54 +0530

    Update DB connection to Clever Cloud

--- staff-dashboard.php
+++ staff-dashboard.php
@@ -16,10 +16,11 @@ if (!isset($_SESSION['staff_username'])) {
 error_log("Staff user logged in: " . $_SESSION['staff_username']);
 
 // Database configuration
-$servername = "localhost";
-$dbusername = "root";
-$dbpassword = "ruban";
-$db = "tekksol_global";
+$servername = "bzbnom7tqqucjcivbuxo-mysql.services.clever-cloud.com";
+$port = "3306";
+$dbusername = "uwgxq8otzk6mhome";
+$dbpassword = "8oQDCXxH6aqYgvkG7g8t";
+$db = "bzbnom7tqqucjcivbuxo";
 
 $staff_data = [];
 $error = "";
